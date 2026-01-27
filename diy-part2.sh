36f5b98e84f188c031fe7aa0897e3b9a3d3ee1b9
zzxxx <120317497+zzzxxxxxxxxxx@users.noreply.github.com>
2026-01-27 18:58:42 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -8,9 +8,8 @@
 # https://github.com/P3TERX/Actions-OpenWrt
 # File name: diy-part2.sh
 # Description: OpenWrt DIY script part 2 (After Update feeds)
-#
 
-# Modify default IP
-sudo apt install libfuse-dev
 rm -rf feeds/packages/lang/golang
 git clone https://github.com/sbwml/packages_lang_golang -b 24.x feeds/packages/lang/golang
+rm -rf feeds/packages/net/speedtest-cli
+rm -rf feeds/packages/net/v2ray-geodata
