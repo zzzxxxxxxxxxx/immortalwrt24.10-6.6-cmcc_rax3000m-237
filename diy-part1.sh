19cabb4ea775e381b813f18476745715b1510d0f
zzxxx <120317497+zzzxxxxxxxxxx@users.noreply.github.com>
2026-01-28 19:38:37 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -9,7 +9,4 @@
 # File name: diy-part1.sh
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
 
-git clone https://github.com/sbwml/luci-app-openlist2 package/openlist
 git clone https://github.com/sirpdboy/luci-app-netspeedtest package/netspeedtest
-git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
-git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
