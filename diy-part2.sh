5d79cc01f8f2e9912200b08ff6437eb195512db6
zzxxx <120317497+zzzxxxxxxxxxx@users.noreply.github.com>
2026-01-28 19:40:27 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -9,7 +9,5 @@
 # File name: diy-part2.sh
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 
-rm -rf feeds/packages/lang/golang
-git clone https://github.com/sbwml/packages_lang_golang -b 24.x feeds/packages/lang/golang
-rm -rf feeds/packages/net/speedtest-cli
 sed -i 's/download-ci-llvm=true/download-ci-llvm=false/g' feeds/packages/lang/rust/Makefile
+rm -rf feeds/packages/net/speedtest-cli
