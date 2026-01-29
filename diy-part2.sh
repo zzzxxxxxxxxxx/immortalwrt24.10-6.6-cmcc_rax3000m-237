29fdca57801480ae81f5ded92fcf69cb2fa42cb6
zzxxx <120317497+zzzxxxxxxxxxx@users.noreply.github.com>
2026-01-29 15:54:49 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,3 +12,5 @@
 rm -rf feeds/packages/net/speedtest-cli
 rm -rf feeds/packages/net/mosdns
 rm -rf feeds/packages/net/v2ray-geodata
+rm -rf feeds/packages/lang/golang
+git clone https://github.com/sbwml/packages_lang_golang -b 24.x feeds/packages/lang/golang
