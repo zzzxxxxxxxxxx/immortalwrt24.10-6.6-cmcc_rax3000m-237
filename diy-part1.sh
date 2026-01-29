22b2fc1f990166ab80d1247773a14fd335c96bad
zzxxx <120317497+zzzxxxxxxxxxx@users.noreply.github.com>
2026-01-29 16:18:20 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -8,6 +8,3 @@
 # https://github.com/P3TERX/Actions-OpenWrt
 # File name: diy-part1.sh
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
-
-git clone --depth 1 https://github.com/sirpdboy/luci-app-netspeedtest package/netspeedtest
-git clone --depth 1 https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
