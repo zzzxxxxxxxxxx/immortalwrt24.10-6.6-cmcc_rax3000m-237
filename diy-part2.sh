0d568dee2c7a156d0420dc5efa6dbdf2aa0f8768
zzxxx <120317497+zzzxxxxxxxxxx@users.noreply.github.com>
2026-01-29 15:21:45 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -10,3 +10,6 @@
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 
 rm -rf feeds/packages/net/speedtest-cli
+rm -rf feeds/packages/lang/golang
+git clone https://github.com/sbwml/packages_lang_golang -b 24.x feeds/packages/lang/golang
+rm -rf feeds/packages/net/v2ray-geodata
