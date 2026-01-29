9d6e8be6472812425a86dd4bf6db3cdcad67f246
zzxxx <120317497+zzzxxxxxxxxxx@users.noreply.github.com>
2026-01-29 16:21:21 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,9 +11,11 @@
 
 rm -rf feeds/packages/net/speedtest-cli
 rm -rf feeds/packages/net/mosdns
-
 rm -rf feeds/packages/net/v2ray-geodata
-git clone --depth 1 https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
 
 rm -rf feeds/packages/lang/golang
 git clone --depth 1 https://github.com/sbwml/packages_lang_golang -b 24.x feeds/packages/lang/golang
+
+git clone --depth 1 https://github.com/sirpdboy/luci-app-netspeedtest package/netspeedtest
+git clone --depth 1 https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
+git clone --depth 1 https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
