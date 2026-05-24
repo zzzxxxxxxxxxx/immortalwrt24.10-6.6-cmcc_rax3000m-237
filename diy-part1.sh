4cf78fc2ff06a650f6cf109ababbc698e379beb3
zzzxxxxxxxxxx <2409974882@qq.com>
2026-05-24 18:51:37 +0800

    移除 luci-app-netspeedtest（DMCA 侵权风波）

--- diy-part1.sh
+++ diy-part1.sh
@@ -9,7 +9,6 @@
 # File name: diy-part1.sh
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
 
-git clone --depth 1 https://github.com/sirpdboy/luci-app-netspeedtest package/netspeedtest
 git clone --depth 1 https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
 git clone --depth 1 https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
 git clone --depth 1 https://github.com/Openwrt-Passwall/openwrt-passwall-packages package/passwall_packages
