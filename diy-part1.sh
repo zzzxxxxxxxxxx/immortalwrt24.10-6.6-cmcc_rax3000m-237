29451d36a6d14ba324685ba05e1127b922a34044
zzzxxxxxxxxxx <2409974882@qq.com>
2026-05-24 20:59:57 +0800

    passwall2 替换为 v2raya

--- diy-part1.sh
+++ diy-part1.sh
@@ -11,5 +11,3 @@
 
 git clone --depth 1 https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
 git clone --depth 1 https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
-git clone --depth 1 https://github.com/Openwrt-Passwall/openwrt-passwall-packages package/passwall_packages
-git clone --depth 1 https://github.com/Openwrt-Passwall/openwrt-passwall2 package/passwall2
