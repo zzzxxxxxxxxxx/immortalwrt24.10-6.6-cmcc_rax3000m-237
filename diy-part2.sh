e54f0103d182b046adaee23ab0ffd7cb361cffd0
zzxxx <120317497+zzzxxxxxxxxxx@users.noreply.github.com>
2026-05-24 22:13:05 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -9,9 +9,7 @@
 # File name: diy-part2.sh
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 
-rm -rf feeds/packages/net/speedtest-cli
-rm -rf feeds/packages/net/mosdns
-rm -rf feeds/packages/net/v2ray-geodata
-
 rm -rf feeds/packages/lang/golang
 git clone --depth 1 https://github.com/sbwml/packages_lang_golang -b 26.x feeds/packages/lang/golang
+
+rm -rf feeds/packages/net/v2ray-geodata
